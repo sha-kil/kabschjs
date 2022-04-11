62035d2fdb0383a24660fb82fd7bb6c8027c07de
ShakilAhmedDhaka <gk.shakil@gmail.com>
2022-04-10 20:25:02 -0400

    added types in test

--- test/getCentroid.test.ts
+++ test/getCentroid.test.ts
@@ -4,12 +4,12 @@ import { getCentroid } from '../src/helper';
 
 describe("computing point set centroid", () => {
   test("computing point set centroid", () => {
-    let point1 = [1, 2, 3];
-    let point2 = [4, 5, 6];
+    let point1: Point = [1, 2, 4];
+    let point2: Point = [3, 6, 10];
 
-    let pointSet = [point1, point2];
+    let pointSet: PointSet = [point1, point2];
 
-    let centroid = getCentroid([[1, 2, 4], [3, 6, 10]]);
+    let centroid = getCentroid(pointSet);
     expect(centroid).toEqual([2, 4, 7]);
 
   });
