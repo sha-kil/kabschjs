a4ae6468c6fe725d12b95054454b6aa829cd4439
ShakilAhmedDhaka <gk.shakil@gmail.com>
2022-04-12 21:58:56 -0400

    prettier added

--- src/helper.ts
+++ src/helper.ts
@@ -5,11 +5,13 @@ import {
   size
 } from 'mathjs';
 
-
 export function getCentroid(points: number[][]) {
   const pointsSize = size(points).valueOf() as Array<number>;
   const numPoints = pointsSize[0];
   const pointsSum = matrixApply(points, 0, matrixSum);
-  const centroid = matrixDivide(pointsSum, numPoints).valueOf() as Array<number>;
+  const centroid = matrixDivide(
+    pointsSum,
+    numPoints
+  ).valueOf() as Array<number>;
   return centroid;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 /**
  * @fileOverView
  * project entry point
- * defines function kabsch
+ * defines function kabsch https://en.wikipedia.org/wiki/Kabsch_algorithm
+ * defines rigid 3D transformation between two sets of points
  */
 
-import { getCentroid } from "./helper";
+import { getCentroid } from './helper';
 import { SVD } from 'svd-js';
 import {
   add as matrixAdd,
@@ -15,10 +16,18 @@ import {
   index,
   subset,
   det
-} from 'mathjs'
+} from 'mathjs';
 
+export function getRigidTransformation(setA: number[][], setB: number[][]) {
+  const rotationalMatrix = kabsch(setA, setB);
+  const translationVector = matrixAdd(
+    matrixMultiply(rotationalMatrix, dotMultiply(getCentroid(setA), -1.0)),
+    getCentroid(setB)
+  );
+  return [rotationalMatrix, translationVector];
+}
 
-export function kabsch(setA: number[][], setB: number[][]) {
+function kabsch(setA: number[][], setB: number[][]) {
   const centroidA = getCentroid(setA);
   const centroidB = getCentroid(setB);
 
@@ -26,21 +35,22 @@ export function kabsch(setA: number[][], setB: number[][]) {
   const centroidBToOrigin = setB.map((row) => matrixSubtract(row, centroidB));
   const centroidAToOriginTransposed = transpose(centroidAToOrigin);
 
-  const covariance = matrixMultiply(centroidAToOriginTransposed, centroidBToOrigin);
+  const covariance = matrixMultiply(
+    centroidAToOriginTransposed,
+    centroidBToOrigin
+  );
   const { u, v } = SVD(covariance, true, true, Number.MIN_VALUE);
   let rotationalMatrix = matrixMultiply(v, transpose(u));
 
   const determinant = det(rotationalMatrix);
   if (determinant < 0) {
     const thirdColumn = subset(rotationalMatrix, index([0, 1, 2], 2));
-    rotationalMatrix = subset(rotationalMatrix, index([0, 1, 2], 2), dotMultiply(thirdColumn, -1));
+    rotationalMatrix = subset(
+      rotationalMatrix,
+      index([0, 1, 2], 2),
+      dotMultiply(thirdColumn, -1)
+    );
   }
 
   return rotationalMatrix;
-}
-
-export function getRigidTransformation(setA: number[][], setB: number[][]) {
-  const rotationalMatrix = kabsch(setA, setB);
-  const translationVector = matrixAdd(matrixMultiply(rotationalMatrix, dotMultiply(getCentroid(setA), -1.0)), getCentroid(setB));
-  return [rotationalMatrix, translationVector];
 }
