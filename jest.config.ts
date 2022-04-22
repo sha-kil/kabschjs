1968d6a11ebf59f58a940ca794f64a545cfb7dc8
Shakil Ahmed <gk.shakil@gmail.com>
2022-04-21 21:36:14 -0400

    ci: test coverage report (#11)
    * ci: test coverage report
    
    Git workflow added to generate test coverage report when pr is created.
    
    * ci: workflow dependency
    
    Test and coverage workflows need to be run after the build
    
    * ci: reusable workflows
    
    Made build and test reusable workflows. Will be calling them from caller
    
    * ci: fix step name
    
    Fixed steps in the caller workflow
    
    * ci: add coverage workflows
    
    Coverage workflow will comment on PRs about test coverage of the branch
    that made the PR

--- jest.config.ts
+++ jest.config.ts
@@ -1,16 +1,21 @@
 // jest.config.ts
-import type { InitialOptionsTsJest } from 'ts-jest'
+import type { InitialOptionsTsJest } from 'ts-jest';
 
 const config: InitialOptionsTsJest = {
-  preset: "ts-jest",
-  testEnvironment: "node",
+  preset: 'ts-jest',
+  testEnvironment: 'node',
   globals: {
     'ts-jest': {
       // ts-jest configuration goes here
-    },
+    }
   },
-  testRegex: "test/.*.\\.(ts)$",
-  coverageProvider: "v8",
-  maxWorkers: "2",
-}
-export default config
+  testRegex: 'test/.*.\\.(ts)$',
+  coverageProvider: 'v8',
+  coverageThreshold: {
+    global: {
+      lines: 80
+    }
+  },
+  maxWorkers: '2'
+};
+export default config;
